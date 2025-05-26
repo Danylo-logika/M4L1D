c8ed4b05ac686ba4fb3bb7c807ad4ca0f44b08b1
BossDan9I <danylo.logika@gmail.com>
2025-05-26 23:17:40 +0300

    Add Model LibraryBook

---
+++ M4L1DA/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class LibaryBook(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=50)
+    available = models.BooleanField()
